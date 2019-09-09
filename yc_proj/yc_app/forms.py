eba1f6afef5dfe844589fb8de0952257a9d9913c
Anthony Leonardi <anthony.leonardi@gmail.com>
2019-09-08 22:31:05 -0400

    integer field instead of charfield.

--- yc_proj/yc_app/forms.py
+++ yc_proj/yc_app/forms.py
@@ -1,12 +1,11 @@
 from django import forms
-from django.forms import ModelForm, IntegerField, NumberInput, TextInput, Textarea, EmailInput, PasswordInput, DateTimeInput, SelectDateWidget, CheckboxInput, modelformset_factory, Select, RadioSelect
 from django.utils import timezone
 
 
 class FetchTreasuryDataForm(forms.Form):
-    year = forms.CharField(required=True, widget=forms.NumberInput(attrs={'class': 'form-control validate', 'min': '1950', 'max': '2022'}))
-    month = forms.CharField(required=False, widget=forms.NumberInput(attrs={'class': 'form-control validate', 'min': '1', 'max': '12'}))
-    day = forms.CharField(required=False, widget=forms.NumberInput(attrs={'class': 'form-control validate', 'min': '1', 'max': '31'}))
+    year = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'class': 'form-control validate', 'min': '1950', 'max': '2022'}))
+    month = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={'class': 'form-control validate', 'min': '1', 'max': '12'}))
+    day = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={'class': 'form-control validate', 'min': '1', 'max': '31'}))
 
     def __init__(self, *args, **kwargs):
         super(FetchTreasuryDataForm, self).__init__(*args, **kwargs)
